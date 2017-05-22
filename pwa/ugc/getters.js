5cdcd6d077c558f1dfa30c646727935ef23a5181
Kevin Miguet <kevin.miguet@dashlane.com>
2017-05-22 11:15:27 +0200

    5/22

---
+++ pwa/ugc/getters.js
@@ -0,0 +1,29 @@
+// Criteria can be [Director|Country|Actor|Genre]
+function filterBy(criteria, value) {
+  return this.movies
+    .filter((movie) => {
+      movie[criteria] === normalize(value);
+    })
+}
+
+// function that returns a list of movies 
+function filterByYear(operator, year) {
+  return this.movies
+    .filter((movie) => {
+      switch (operator) {
+        case "equals":
+          return movie.year === year;
+        case "before":
+          return movie.year < year;
+        case "after": 
+          return movie.year > year;
+        default:
+          console.log ("ERROR: unknown operator. Filter won't be applied")
+          return true
+      }
+    })
+}
+
+function getOldMovies() {
+  return this.filterByYear('before', db.currentYear);
+}
--- pwa/ugc/main.js
+++ pwa/ugc/main.js
@@ -1,37 +1,36 @@
-function hideMenu(){
+function hideMenu() {
   $("#message").width("0%");
 }
 
-function showMenu(element){
-  $(".menuInfo").hide();  
+function showMenu(element) {
+  $(".menuInfo").hide();
   $(`#${element.dataName}`).show();
   $("#message").width("30%");
 }
 
 const joursSemaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
 const googleMapConfig = {
-    zoom: 12,
-    center: {"lat": 48.858949,"lng": 2.346373 },
-    zoomControl: false,
-    mapTypeControl: false,
-    scaleControl: false,
-    streetViewControl: false,
-    rotateControl: false,
-    fullscreenControl: false,
-    clickableIcons: false,
-    gestureHandling: "greedy",
-  }
+  zoom: 12,
+  center: { "lat": 48.858949, "lng": 2.346373 },
+  zoomControl: false,
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  rotateControl: false,
+  fullscreenControl: false,
+  clickableIcons: false,
+  gestureHandling: "greedy",
+}
 
 const generateMenu = (cinema) => {
-    let message = "";
-    message += `<div class="menuInfo" id="${cinema.dataName}" ><h1>${cinema.name}</h1>\n`
-  if (schedules.results[cinema.dataName] && schedules.results[cinema.dataName].length != 0) {
-    schedules.results[cinema.dataName].forEach((film) => {
-      console.log(film)
+  let message = "";
+  message += `<div class="menuInfo" id="${cinema.dataName}" ><h1>${cinema.name}</h1>\n`
+  if (movieData.schedules[cinema.dataName] && movieData.schedules[cinema.dataName].length != 0) {
+    movieData.schedules[cinema.dataName].forEach((film) => {
       message += `<li class="mdl-list__item mdl-list__item--two-line">
-        <img src="https://image.tmdb.org/t/p/original${schedules.posters[film.title]}" width="128" height="170">
+        <img src="https://image.tmdb.org/t/p/original${movieData.movies[film.id].data}" width="128" height="170">
         <span class="mdl-list__item-primary-content">
-         <span>${film.title}</span>`;
+         <span>${movieData.movies[film.id].title}</span>`;
       joursSemaine.forEach((jour) => {
         if (film.schedule[jour]) {
           message += `<span class="mdl-list__item-sub-title">${jour} : ${film.schedule[jour]}</span>`;
@@ -40,20 +39,20 @@ const generateMenu = (cinema) => {
       message += `</span><span class="mdl-list__item-secondary-content"></span></li>`
     });
   }
-  message+=`</div>`
+  message += `</div>`
   $("#movieList").append(message);
 }
 
-function generateMarker (element, map) {
+function generateMarker(element, map) {
   var marker = new google.maps.Marker({
     position: element.position,
     map: map,
     icon: new google.maps.MarkerImage(
-    "./images/marker.png",
-    null, /* size is determined at runtime */
-    null, /* origin is 0,0 */
-    null, /* anchor is bottom center of the scaled image */
-    new google.maps.Size(45, 45)),
+      "./images/marker.png",
+      null, /* size is determined at runtime */
+      null, /* origin is 0,0 */
+      null, /* anchor is bottom center of the scaled image */
+      new google.maps.Size(45, 45)),
   });
   // show the menu when clicking the marker
   marker.addListener('click', () => {
@@ -65,7 +64,7 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), googleMapConfig);
   // hide the menu when clicking the map
   google.maps.event.addListener(map, "click", () => {
-    hideMenu();   
+    hideMenu();
   })
   // generate markers
   cinemas.forEach((cinema) => {
@@ -73,3 +72,17 @@ function initMap() {
     generateMenu(cinema);
   });
 }
+
+function generateMovieList() {
+  let message = "";
+  for (let movie in movieData.movies) {
+    message += `<li class="mdl-list__item mdl-list__item--two-line">
+        <img src="https://image.tmdb.org/t/p/original${movieData.movies[movie].data}" width="128" height="170">
+        <span class="mdl-list__item-primary-content">
+         <span>${movieData.movies[movie].title}</span>`;
+    message += `<span class="mdl-list__item-sub-title">hohoh</span>`;
+    message += `</span><span class="mdl-list__item-secondary-content"></span></li>`
+  }
+  $("#movieList2").append(message);
+}
+
